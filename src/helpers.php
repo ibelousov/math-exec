a8d689d8b23d0afd4d68adb53b1daf8176d9df28
ivanbelousov <i.a.belousov.88@gmail.com>
2020-04-04 17:36:03 +0300

    clean

--- src/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function math_exec($expression, $inner_precision = 40)
-{
-    $evaluator = new \Ibelousov\MathExec\Evaluator\Evaluator($expression, $inner_precision);
-
-    return $evaluator->exec()->value;
-}
